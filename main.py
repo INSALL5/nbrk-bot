57163b0dfa73f64804ec02cdbca2018bc99987cb
INSALL5 <insar1234567@gmail.com>
2025-04-17 16:49:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-
 from flask import Flask
 import subprocess
+import threading
 
 app = Flask(__name__)
 
-@app.route("/")
 def run_script():
     subprocess.run(["python3", "nbrk_rates.py"])
-    return "Курсы валют обновлены!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+@app.route("/")
+def index():
+    threading.Thread(target=run_script).start()
+    return "🕒 Обработка запущена"
