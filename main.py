49b5d9811c2d2ae5f0a066156583f20fd230f6a1
INSALL5 <insar1234567@gmail.com>
2025-04-17 17:21:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from flask import Flask
 import subprocess
 import threading
-import traceback
 
 app = Flask(__name__)
+is_running = False  # флаг для защиты
 
 def run_script():
+    global is_running
+    if is_running:
+        print("⏳ Запуск уже выполняется — пропуск")
+        return
+
+    is_running = True
     try:
         print("▶️ Запуск скрипта nbrk_rates.py")
         subprocess.run(["python3", "nbrk_rates.py"])
-        print("✅ Скрипт завершён")
+        print("✅ Завершено")
     except Exception as e:
-        print("‼️ Ошибка при запуске скрипта:", e)
-        traceback.print_exc()
+        print("‼️ Ошибка:", e)
+    finally:
+        is_running = False
 
 @app.route("/")
 def index():
