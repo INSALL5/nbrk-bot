bed10503d588254a46afc715a2a27d17b359fc1a
INSALL5 <insar1234567@gmail.com>
2025-04-17 16:27:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ def run_script():
     subprocess.run(["python3", "nbrk_rates.py"])
     return "Курсы валют обновлены!"
 
-app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
